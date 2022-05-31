ef58b316b7e75d1ce7b8a1747dd9823350b8a460
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2022-05-31 18:00:22 +0530

    Navigation Added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Header from '../components/header'
 
 export default function Home() {
   return (
-    <h1 className="text-6xl font-bold underline text-indigo-500">
-      Hello world!
-    </h1>
+   <div>
+      <Header></Header>
+   </div>
   )
 }
