16fc3bcf0cf363d44df8510c35aadac44fe5a592
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2022-06-04 16:57:38 +0530

    Footer Added

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,31 @@
+import { ImFacebook, ImTwitter, ImYoutube } from "react-icons/im";
+import Link from 'next/link'
+import Newslatter from "./_child/newslatter";
 
 export default function footer() {
+
+  const bg = {
+    backgroundImage : "url('/images/footer.png')",
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: "bottom left"
+  }
+
   return (
-    <div className="text-3xl">footer</div>
+    <footer className="bg-gray-50" style={bg}>
+      <Newslatter></Newslatter>
+      <div className="container mx-auto flex justify-center py-12">
+          <div className="py-5">
+              <div className="flex gap-6 justify-center">
+                    <Link href={"/"}><a><ImFacebook color="#888888" /></a></Link>
+                    <Link href={"/"}><a><ImTwitter color="#888888" /></a></Link>                    
+                    <Link href={"/"}><a><ImYoutube color="#888888" /></a></Link>
+              </div>
+
+              <p className="py-5 text-gray-400">Copyright ©2022 All rights reserved | This template is made with  by Daily Tuition</p>
+              <p className="text-gray-400 text-center">Terms & Condition</p>
+          </div>
+      </div>
+
+    </footer>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Format from '../layout/format';
 import Section1 from '../components/section1';
 import Section2 from '../components/section2';
 import Section3 from '../components/section3';
+import Section4 from '../components/section4';
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <Section1></Section1>
       <Section2></Section2>
       <Section3></Section3>
+      <Section4></Section4>
    </Format>
   )
 }
