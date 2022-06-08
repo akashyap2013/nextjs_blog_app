661a3578095ea4138aff7733eecd4bc6006993b5
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2022-06-08 18:19:02 +0530

    SWR Added

--- components/section2.js
+++ components/section2.js
@@ -1,11 +1,13 @@
 import Link from "next/link"
 import Image from "next/image"
 import Author from "./_child/author"
-import getPost from "../lib/helper"
+import fetcher from '../lib/fetcher'
+
 
 export default function section2() {
 
-    getPost(4).then(res => console.log(res))
+    const { data, isLoading, isError } = fetcher('api/posts')
+    if(data) console.log(data)
   return (
     <section className="container mx-auto md:px-20 py-10">
         <h1 className="font-bold text-4xl py-12 text-center">Latest Posts</h1>
