d975012483bf6963689e33ff4542387df301a93b
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2022-06-08 17:47:55 +0530

    Fetching

--- components/section2.js
+++ components/section2.js
@@ -1,8 +1,11 @@
 import Link from "next/link"
 import Image from "next/image"
 import Author from "./_child/author"
+import getPost from "../lib/helper"
 
 export default function section2() {
+
+    getPost(4).then(res => console.log(res))
   return (
     <section className="container mx-auto md:px-20 py-10">
         <h1 className="font-bold text-4xl py-12 text-center">Latest Posts</h1>
