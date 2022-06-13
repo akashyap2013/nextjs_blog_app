f49f28c60bc6b75158c58c482d30bbf3f79a670c
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2022-06-13 19:54:18 +0530

    Final Commit

--- components/section2.js
+++ components/section2.js
@@ -48,7 +48,7 @@ function Post( { data } ){
                     Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind 
                     text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
                 </p>
-                { author ? <Author></Author> : <></>}
+                { author ? <Author {...author}></Author> : <></>}
             </div>
         </div>
     )
